51ec367a0c2f2c5a511b336001784da463320e39
neilkapadia7 <neilkapadia7@gmail.com>
2020-04-25 00:19:04 +0530

    User Posts Commit

--- routes/posts.js
+++ routes/posts.js
@@ -56,4 +56,58 @@ router.post(
 	}
 );
 
+// @route   PUT    api/posts
+// @desc    Update Post
+// @access  Private
+router.put("/:id", auth, async (req, res) => {
+	const { body } = req.body;
+
+	const postfields = {};
+
+	if (body) postfields.body = body;
+
+	try {
+		let post = await Post.findById(req.params.id);
+
+		if (!post) return res.status(404).json({ msg: "Post Not Found" });
+
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: "Not Authorized" });
+		}
+
+		post = await Post.findByIdAndUpdate(
+			req.params.id,
+			{ $set: postfields },
+			{ new: true }
+		);
+
+		res.json(post);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route   DELETE    api/posts
+// @desc    Delete Post
+// @access  Private
+router.delete("/:id", auth, async (req, res) => {
+	try {
+		let post = await Post.findById(req.params.id);
+
+		if (!post) return res.status(404).json({ msg: "Post Not Found" });
+
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: "Not Authorized" });
+		}
+
+		await Post.findByIdAndRemove(req.params.id);
+
+		res.json({ msg: "Post Deleted" });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
+});
+
 module.exports = router;
