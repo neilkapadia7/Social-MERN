d9d0783e1aedf3c02ae3b787f5a7697e4e1d592b
neilkapadia7 <neilkapadia7@gmail.com>
2020-04-25 00:36:43 +0530

    Posts Commit

--- routes/posts.js
+++ routes/posts.js
@@ -110,4 +110,32 @@ router.delete("/:id", auth, async (req, res) => {
 	}
 });
 
+// @route   GET    api/posts/all
+// @desc    Get all Posts
+// @access  Public
+router.get("/all", auth, async (req, res) => {
+	try {
+		const posts = await Post.find().sort({ date: -1 });
+
+		res.json(posts);
+	} catch (error) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route   GET    api/posts/user/:id
+// @desc    Get user's all Posts
+// @access  Public
+router.get("/user/:id", auth, async (req, res) => {
+	try {
+		const post = await Post.find({ user: req.params.id }).sort({ date: -1 });
+
+		res.json(post);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server Error");
+	}
+});
+
 module.exports = router;
