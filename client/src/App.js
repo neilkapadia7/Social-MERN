c3fe38be060fa26590e360811cc378c5795aea5c
neilkapadia7 <neilkapadia7@gmail.com>
2020-04-25 14:26:05 +0530

    Redux Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
+
 const App = () => {
-	return <div className='App'></div>;
+	return (
+		<Provider store={store}>
+			<div className='App'></div>
+		</Provider>
+	);
 };
 
 export default App;
